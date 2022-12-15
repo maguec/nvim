57559111a6892071e5057a81aab90db9ca6c14b9
Chris Mague <mague@google.com>
2022-12-15 13:44:27 -0800

    add in terraform plugins

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -12,3 +12,18 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
+
+---------------------------------------------------------------------
+-- Terraform
+---------------------------------------------------------------------
+vim.cmd([[silent! autocmd! filetypedetect BufRead,BufNewFile *.tf]])
+vim.cmd([[autocmd BufRead,BufNewFile *.hcl set filetype=hcl]])
+vim.cmd([[autocmd BufRead,BufNewFile .terraformrc,terraform.rc set filetype=hcl]])
+vim.cmd([[autocmd BufRead,BufNewFile *.tf,*.tfvars set filetype=terraform]])
+vim.cmd([[autocmd BufRead,BufNewFile *.tfstate,*.tfstate.backup set filetype=json]])
+vim.cmd([[let g:terraform_fmt_on_save=1]])
+vim.cmd([[let g:terraform_align=1]])
+vim.keymap.set("n", "<leader>ti", ":!terraform init<CR>", opts)
+vim.keymap.set("n", "<leader>tv", ":!terraform validate<CR>", opts)
+vim.keymap.set("n", "<leader>tp", ":!terraform plan<CR>", opts)
+vim.keymap.set("n", "<leader>taa", ":!terraform apply -auto-approve<CR>", opts)
