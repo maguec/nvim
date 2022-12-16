1d7e02cec22933863fb20cdc904d2ddf8ed9f30e
Chris Mague <mague@google.com>
2022-12-16 11:42:22 -0800

    add calendars

--- lua/vimwiki.lua
+++ lua/vimwiki.lua
@@ -11,3 +11,24 @@ vim.cmd([[ let g:vimwiki_list = [wiki_google] ]])
 
 -- only configure md as wiki in the vimwiki paths
 vim.cmd([[let g:vimwiki_global_ext = 0]])
+
+vim.api.nvim_exec([[
+function! ToggleCalendar()
+  execute ":Calendar"
+  if exists("g:calendar_open")
+    if g:calendar_open == 1
+      execute "q"
+      unlet g:calendar_open
+    else
+      g:calendar_open = 1
+    end
+  else
+    let g:calendar_open = 1
+  end
+endfunction
+]], false)
+
+vim.cmd([[autocmd FileType vimwiki map <leader>c :call ToggleCalendar()<CR>]])
+vim.cmd([[autocmd FileType vimwiki map <leader>s :VimwikiSearch /]])
+vim.cmd([[autocmd FileType vimwiki map <leader>dg :VimwikiDiaryGenerateLinks<CR>]])
+vim.cmd([[autocmd FileType vimwiki map <leader>di :VimwikiDiaryIndex<CR>]])
