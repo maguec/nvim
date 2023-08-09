5d7a2cea7227fcdabfc0f086a869696dc792012a
Chris Mague <github@mague.com>
2023-08-09 15:00:07 -0700

    it is already set to two so just match it

--- lua/myfunctions.lua
+++ lua/myfunctions.lua
@@ -4,7 +4,7 @@ M.togglenums = function()
 if (vim.wo.number == false and vim.wo.relativenumber == false) then
      vim.wo.number = true
      vim.wo.relativenumber = true
-     vim.wo.signcolumn = "auto"
+     vim.wo.signcolumn = "yes:2"
 elseif( vim.wo.number == true and vim.wo.relativenumber == true) then
      vim.wo.number = false
      vim.wo.relativenumber = false
