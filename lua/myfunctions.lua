0bac71f888c422120906d822bb1ea6ec210a7358
Chris Mague <github@mague.com>
2023-08-09 14:57:14 -0700

    get rid of the gutters as well

--- lua/myfunctions.lua
+++ lua/myfunctions.lua
@@ -4,10 +4,11 @@ M.togglenums = function()
 if (vim.wo.number == false and vim.wo.relativenumber == false) then
      vim.wo.number = true
      vim.wo.relativenumber = true
-     --vim.echo "nu:1/rnu:0"
+     vim.wo.signcolumn = "auto"
 elseif( vim.wo.number == true and vim.wo.relativenumber == true) then
      vim.wo.number = false
      vim.wo.relativenumber = false
+     vim.wo.signcolumn = "no"
 else
      vim.api.nvim_command('echo "something is broke yo!"')
 end
