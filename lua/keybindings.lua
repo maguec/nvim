c4ba7365fe0823c162c1a1bf2f66bdd32f0450ce
Chris Mague <github@mague.com>
2023-08-10 11:30:33 -0700

    add a close keybinding

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -35,4 +35,5 @@ local myfunctions = require "myfunctions"
 vim.keymap.set('n', '<leader>tt', myfunctions.togglenums, {})
 -- Toggle the LSP recommendations
 vim.keymap.set("n", "<leader>xx", function() require("trouble").open() end)
+vim.keymap.set("n", "<leader>xc", function() require("trouble").close() end)
 
