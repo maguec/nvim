52b80699bcad573c792e49115d7c08021d52b69b
Chris Mague <github@mague.com>
2023-11-30 09:08:37 -0800

    add whichkey

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -66,6 +66,19 @@ return require('packer').startup(function(use)
   	requires = 'kyazdani42/nvim-web-devicons',
   }
 
+  use {
+    'folke/which-key.nvim',
+    config = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+      require("which-key").setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+  end
+  }
+
   --------------------------------------------
   --   File exporter
   --------------------------------------------
