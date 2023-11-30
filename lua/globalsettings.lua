6fb92e8a71963a120961edbfec9e37d98299dace
Chris Mague <mague@google.com>
2023-11-30 08:57:53 -0800

    merge gui configs

--- lua/globalsettings.lua
+++ lua/globalsettings.lua
@@ -23,5 +23,10 @@ vim.cmd[[colorscheme tokyonight-night]]
 
 -- neovide only settings
 if vim.g.neovide then
-    o.guifont = "DroidSansM Nerd Font Mono:h14"
+    o.guifont = "DroidSansMono Nerd Font Mono:h18"
+    vim.g.neovide_input_use_logo = 1
+    vim.api.nvim_set_keymap('', '<D-v>', '+p<CR>', { noremap = true, silent = true})
+    vim.api.nvim_set_keymap('!', '<D-v>', '<C-R>+', { noremap = true, silent = true})
+    vim.api.nvim_set_keymap('t', '<D-v>', '<C-R>+', { noremap = true, silent = true})
+    vim.api.nvim_set_keymap('v', '<D-v>', '<C-R>+', { noremap = true, silent = true})
 end
